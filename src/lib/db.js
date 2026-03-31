d45d75570e0843b9f2815a60be2f80cbad4daf67
ghifar1327 <givartorreto@gmail.com>
2026-03-31 18:03:44 +0700

    feat(db): add PostgreSQL pool configuration with env support

---
+++ src/lib/db.js
@@ -0,0 +1,16 @@
+
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+});
+async function db() {
+    const client = await pool.connect()
+    return client
+}
+
+export default db
