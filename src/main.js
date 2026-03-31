401cb9f22608fedcd6af679cee2bf91cb5391a04
ghifar1327 <givartorreto@gmail.com>
2026-03-31 13:11:48 +0700

    feat: setup express app with user routes

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import express from "express"
+import userRouter from "./routes/users.router.js"
+const app = express()
+
+app.use(express.json())
+app.use("/users", userRouter)
+app.get("/", function(req, res){
+    res.json({
+        success : true,
+        message: "backend is running"
+    })
+})
+
+app.listen(9999, function(){
+    console.log("App listening on port 9999")
+})
