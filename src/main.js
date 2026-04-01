82a6e6ed11d737fcb4ebb77cfa3022e2f13b052d
ghifar1327 <givartorreto@gmail.com>
2026-04-01 22:02:13 +0700

    feat: add admin route with auth middleware

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import express from "express";
 import db from "./lib/db.js";
 import adminRouter from "./routes/admin.router.js";
 import authRouter from "./routes/auth.router.js";
+import auth from "./middleware/auth.middleware.js";
 
 export const conn = db();
 const app = express();
 
 app.use(express.json());
 
-app.use("", adminRouter);
+app.use("admin",auth ,adminRouter);
 app.use("/auth", authRouter);
 app.get("/", function(req, res){
     res.json({
