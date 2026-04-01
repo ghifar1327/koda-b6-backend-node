7f48bfbd973cc25cd856ac2577ddfd3c6dc2a6a2
ghifar1327 <givartorreto@gmail.com>
2026-04-01 21:10:12 +0700

    feat(model): implement forgot password OTP flow

---
+++ src/models/auth.models.js
@@ -0,0 +1,31 @@
+import { pool } from "../lib/db.js";
+import { GenerateOTP } from "../lib/otp.js";
+
+export async function createForgotPwd(email) {
+    const otp = GenerateOTP();
+    const result = await pool.query(
+        `INSERT INTO forgot_password (email, code) VALUES ($1, $2)
+        RETURNING email, code;`,
+        [email, otp]
+    );
+    return result.rows[0];
+}
+
+export async function getForgotPwdByEmail(email) {
+    const result = await pool.query(`SELECT 
+		email,
+		code
+		FROM forgot_password WHERE email=$1
+        RETURNING email, code;
+        `,
+        [email]        
+    );
+    return await result.rows[0];
+}
+
+export async function deleteForgotPwd(email) {
+    await pool.query(`
+        DELETE FROM forgot_password WHERE email=$1`, 
+        [email]
+    );
+}
