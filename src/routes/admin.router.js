74b6ee7a8d079fef2e063e714378dea19531e953
ghifar1327 <givartorreto@gmail.com>
2026-04-12 01:29:05 +0700

    feat: implementation swager docs admin route

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -3,6 +3,49 @@ import userRouter from "./users.router.js";
 
 const adminRouter = Router();
 
+/**
+ * @swagger
+ * /admin/users:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Admin]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *       400:
+ *         description: Bad request
+ */
+
+/**
+ * @swagger
+ * /admin/users:
+ *   get:
+ *     summary: Get all users
+ *     tags: [Admin]
+ *     responses:
+ *       200:
+ *         description: List of all users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
+
 adminRouter.use("/users", userRouter);
 
 export default adminRouter;
