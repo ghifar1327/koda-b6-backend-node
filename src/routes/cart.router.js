93ba45187bec0b43cf615e27622ae49be223f13f
ghifar1327 <givartorreto@gmail.com>
2026-04-12 01:29:39 +0700

    feat: implementation swager docs chart route

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -3,9 +3,104 @@ import * as cartController from "../controller/cart.controller.js";
 
 const cartRouter = express.Router();
 
+/**
+ * @swagger
+ * /cart:
+ *   post:
+ *     summary: Add item to cart
+ *     tags: [Cart]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user_id:
+ *                 type: integer
+ *               product_id:
+ *                 type: integer
+ *               size_id:
+ *                 type: integer
+ *               variant_id:
+ *                 type: integer
+ *               quantity:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Item added to cart successfully
+ *       400:
+ *         description: Bad request
+ */
 cartRouter.post("/", cartController.addToCart);
-cartRouter.get("/:user_id", cartController.getCartByUser);
+
+/**
+ * @swagger
+ * /cart/{user_id}:
+ *   get:
+ *     summary: Get cart by user ID
+ *     tags: [Cart]
+ *     parameters:
+ *       - in: path
+ *         name: user_id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: User's cart items
+ *       404:
+ *         description: User not found
+ */
+cartRouter.get("/:user_id", cartController.getCartByIdUser);
+
+/**
+ * @swagger
+ * /cart/{id}:
+ *   put:
+ *     summary: Update cart item
+ *     tags: [Cart]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               quantity:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Cart item updated
+ *       404:
+ *         description: Cart item not found
+ */
 cartRouter.put("/:id", cartController.updateCart);
+
+/**
+ * @swagger
+ * /cart/{id}:
+ *   delete:
+ *     summary: Delete cart item
+ *     tags: [Cart]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Cart item deleted
+ *       404:
+ *         description: Cart item not found
+ */
 cartRouter.delete("/:id", cartController.deleteCart);
 
 export default cartRouter;
