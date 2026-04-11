fd01dec63d390692342bf8144193e6c90e287fa9
ghifar1327 <givartorreto@gmail.com>
2026-04-12 01:30:52 +0700

    feat: implementation swager docs  transaction route route

--- src/routes/transactions.router.js
+++ src/routes/transactions.router.js
@@ -3,11 +3,138 @@ import * as transactionController from "../controller/transaction.controller.js"
 
 const transactionRouter = express.Router();
 
+/**
+ * @swagger
+ * /transactions:
+ *   post:
+ *     summary: Create a new transaction
+ *     tags: [Transactions]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: integer
+ *               productId:
+ *                 type: integer
+ *               quantity:
+ *                 type: integer
+ *     responses:
+ *       201:
+ *         description: Transaction created successfully
+ *       400:
+ *         description: Bad request
+ */
 transactionRouter.post("/", transactionController.createTansaction);
+
+/**
+ * @swagger
+ * /transactions:
+ *   get:
+ *     summary: Get all transactions
+ *     tags: [Transactions]
+ *     responses:
+ *       200:
+ *         description: List of transactions
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
 transactionRouter.get("/", transactionController.getAllTransactions);
+
+/**
+ * @swagger
+ * /transactions/{id}:
+ *   get:
+ *     summary: Get transaction by ID
+ *     tags: [Transactions]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Transaction details
+ *       404:
+ *         description: Transaction not found
+ */
 transactionRouter.get("/:id", transactionController.getTransactionById);
+
+/**
+ * @swagger
+ * /transactions/{id}:
+ *   patch:
+ *     summary: Update transaction
+ *     tags: [Transactions]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               status:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Transaction updated
+ *       404:
+ *         description: Transaction not found
+ */
 transactionRouter.patch("/:id", transactionController.updateTransaction);
+
+/**
+ * @swagger
+ * /transactions/{id}:
+ *   delete:
+ *     summary: Delete transaction
+ *     tags: [Transactions]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Transaction deleted
+ *       404:
+ *         description: Transaction not found
+ */
 transactionRouter.delete("/:id", transactionController.deleteTransaction);
+
+/**
+ * @swagger
+ * /transactions/{userId}:
+ *   get:
+ *     summary: Get transactions by user ID
+ *     tags: [Transactions]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: List of user's transactions
+ *       404:
+ *         description: User not found
+ */
 transactionRouter.get("/:userId", transactionController.getTransactionsByUserId);
 
 export default transactionRouter;
