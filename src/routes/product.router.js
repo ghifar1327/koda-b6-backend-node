ec41e4b244e3f41a0ded355e20db61d25b7da964
ghifar1327 <givartorreto@gmail.com>
2026-04-12 01:30:30 +0700

    feat: implementation swager docs  product route

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -3,15 +3,162 @@ import * as productController from "../controller/product.controller.js";
 
 const productRouter = express.Router();
 
-// PUBLIC
+/**
+ * @swagger
+ * /product:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: List of all products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
 productRouter.get("/", productController.getAllProducts);
+
+/**
+ * @swagger
+ * /product/{id}:
+ *   get:
+ *     summary: Get product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Product details
+ *       404:
+ *         description: Product not found
+ */
 productRouter.get("/:id", productController.getProductById);
+
+/**
+ * @swagger
+ * /product/{id}/variants:
+ *   get:
+ *     summary: Get variants by product ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Product variants
+ *       404:
+ *         description: Product not found
+ */
 productRouter.get("/:id/variants", productController.getVariantsByProductId);
+
+/**
+ * @swagger
+ * /product/{id}/sizes:
+ *   get:
+ *     summary: Get sizes by product ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Product sizes
+ *       404:
+ *         description: Product not found
+ */
 productRouter.get("/:id/sizes", productController.getSizesByProductId);
 
-// PRIVATE (ADMIN)
+/**
+ * @swagger
+ * /product:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *     responses:
+ *       201:
+ *         description: Product created successfully
+ *       400:
+ *         description: Bad request
+ */
 productRouter.post("/", productController.createProduct);
+
+/**
+ * @swagger
+ * /product/{id}:
+ *   put:
+ *     summary: Update product
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Product updated
+ *       404:
+ *         description: Product not found
+ */
 productRouter.put("/:id", productController.updateProduct);
+
+/**
+ * @swagger
+ * /product/{id}:
+ *   delete:
+ *     summary: Delete product
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Product deleted
+ *       404:
+ *         description: Product not found
+ */
 productRouter.delete("/:id", productController.deleteProduct);
 
 export default productRouter;
