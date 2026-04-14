2749ea60e2d7db62b4f4d4d4665263501513466e
ghifar1327 <givartorreto@gmail.com>
2026-04-14 14:40:09 +0700

    refactor(config): replace hardcoded DB config with DATABASE_URL env

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,11 +2,7 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT
+  connectionString: process.env.DATABASE_URL,
 });
 async function db() {
     const client = await pool.connect();
