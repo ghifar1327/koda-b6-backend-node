27cf3ce7a279dd38d730b8bba947da4f62146540
ghifar1327 <givartorreto@gmail.com>
2026-04-14 10:30:43 +0700

    fix: add bearer token validation for authorization header

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,25 +1,37 @@
 import { verifyToken } from "../lib/jwt.js";
-import {constants} from "http2";
 /**
  * 
  * @param {import{"express"}.request} req 
  * @param {import{"express"}.response} res 
  * @param {import{"express"}.NexFunction} next 
  */
-export default function auth(req, res, next){
-    const authHeader = req.header.authorization;
-    const prefix = "Bearer";
-    const isBearer = authHeader.startsWite(prefix);
-    if(isBearer){
-        const token = authHeader.slice(prefix.length);
-        const payload = verifyToken(token);
-        if(payload){
-            res.locals = payload;
-            return next();
-        }
-    }
-    return res.status(constants.HTTP_STATUS_UNAUTHORIZED).json({
-        success: false,
-        message: "unauthorized" 
+export default function auth(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized",
     });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = verifyToken(token);
+
+    if (!payload) {
+      throw new Error("Invalid token");
+    }
+
+    res.locals.user = payload;
+
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status().json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
 }
