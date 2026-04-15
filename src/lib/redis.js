8b9dfc3cd0b104609b111c423e560d7bcabee1ea
ghifar1327 <givartorreto@gmail.com>
2026-04-15 17:13:29 +0700

    fix: create redis client used socket

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -8,7 +8,11 @@ export async function initRedis() {
   }
 
   redisClient = createClient({
-  url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    },
+    password: process.env.REDIS_PASSWORD,
   });
 
   redisClient.on("error", (err) => {
