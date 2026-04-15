e6a2ac9324533c631ade2bf2ee5903cf44b517f6
ghifar1327 <givartorreto@gmail.com>
2026-04-15 14:54:54 +0700

    fix: add logic patch for update user,  response error message

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -15,6 +15,9 @@ export async function createUser(req, res) {
         const reqData = { ...req.body, password: hash };
         console.log(reqData);
         const user = await userModels.createUser(reqData);
+
+        if (!user) throw new Error("Failed to create user" );
+        
         res.status(constants.HTTP_STATUS_CREATED).json({
             success: true,
             message: "register success",
@@ -23,8 +26,7 @@ export async function createUser(req, res) {
     } catch (error) {
         res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
             success: false,
-            message: "failed to register",
-            error: error.message,
+            message: error.message,
         });
     }
 }
@@ -34,12 +36,20 @@ export async function createUser(req, res) {
  * @param {import("express").Response} res
 */
 export async function getAllUser(req,res) {
+    try{
     const users = await userModels.getAllUsers();
-    res.json({
+    if (!users) throw new Error("Users not found" );
+    res.status(constants.HTTP_STATUS_OK).json({
         success: true,
         message: "list of users",
         result: users
-    });
+        });
+    } catch (error) {
+    res.status(constants.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: error.message,
+        }); 
+    }
 }
 
 
@@ -61,32 +71,62 @@ export async function getUserById(req, res) {
     }
 }
 
-export async function UpdateUser(req, res) {
-    try {
-      const id = req.params.id;
-      const user = userModels.updateUser(id, req.body);
-      if (!user) throw new Error("User not found" );
-      
-        res.status(200).json({
-          success: true,
-          message: "updated successfuly",
-          resust: user
-      });
-    } catch (err) {
-      res.status(404).json({ message: err.message });
+export async function updateUser(req, res) {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+
+    // get previous data user by id
+    const existing = await userModels.getUserByid(id);
+    if (!existing) {
+      throw new Error("User not found");
     }
+
+    // merge data with previous data
+    const payload = {
+      picture: data.picture ?? existing.picture,
+      full_name: data.full_name ?? existing.full_name,
+      email: data.email ?? existing.email,
+      password: data.password ?? existing.password,
+      address: data.address ?? existing.address,
+      phone: data.phone ?? existing.phone,
+      role: data.role ?? existing.role,
+    };
+
+    const user = await userModels.updateUser(id, payload);
+    if (!user) {
+      throw new Error("Failed to update user");
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      results: user,
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: err.message,
+    });
+  }
 }
 
 export async function deleteUser(req, res) {
     try{
       const id = req.params.id;
-      const user = userModels.deleteUser(id);
-        res.status(200).json({
+      const user = await userModels.deleteUser(id);
+    
+      if (!user) throw new Error("User not found" );
+
+      res.status(200).json({
         success: true,
         message: "deleted successfuly",
-        resust: user
+        result: user
       });
     }catch(err){
-        res.status(404).json({ message: err.message });
+        res.status(404).json({
+            success: false,
+            message: err.message,
+        });
     }
 }
