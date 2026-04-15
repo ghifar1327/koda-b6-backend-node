774db04a799896ab4a2a6ddd3b81f4cfadc22c8d
ghifar1327 <givartorreto@gmail.com>
2026-04-16 01:10:55 +0700

    feat: add update picture models

--- src/models/auth.models.js
+++ src/models/auth.models.js
@@ -36,3 +36,60 @@ export async function updatePassword(email, pwd) {
     );
     return result.rows[0];
 }
+
+
+export const updateUserPicture = async (id, picture) => {
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    // UPDATE
+    const updateResult = await client.query(
+      `
+      UPDATE users 
+      SET 
+        picture = $1,
+        updated_at = $2
+      WHERE id = $3
+      `,
+      [picture, new Date(), id]
+    );
+
+    if (updateResult.rowCount === 0) {
+      throw new Error("User not found");
+    }
+
+    // SELECT
+    const result = await client.query(
+      `
+      SELECT 
+        u.id, 
+        u.full_name, 
+        u.picture, 
+        u.email,
+        u.password,
+        r.name as role, 
+        u.phone, 
+        u.address, 
+        u.created_at, 
+        u.updated_at 
+      FROM users u
+      JOIN role r ON u.role_id = r.id
+      WHERE u.id = $1
+      `,
+      [id]
+    );
+
+    const user = result.rows[0];
+
+    await client.query("COMMIT");
+
+    return user;
+  } catch (err) {
+    await client.query("ROLLBACK");
+    throw err;
+  } finally {
+    client.release();
+  }
+};
